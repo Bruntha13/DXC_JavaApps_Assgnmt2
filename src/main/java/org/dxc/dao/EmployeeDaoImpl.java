852f0c61f86ec84692e7dc99b655b9c60ec4f686
Bruntha13 <117891177+Bruntha13@users.noreply.github.com>
2023-06-01 23:52:55 +0530

    EmployeeDaoImpl.java

--- src/main/java/org/dxc/dao/EmployeeDaoImpl.java
+++ src/main/java/org/dxc/dao/EmployeeDaoImpl.java
@@ -13,43 +13,31 @@ public int saveEmployee(String name, int age, double salary) {
 		employee.setEmployeeName(name);
 		employee.setEmployeeAge(age);
 		employee.setEmployeeSalary(salary);
-		
 		Session session = HibernateUtil.getSessionFactory().openSession();
-		
 		session.beginTransaction();
-		
 		int id = (Integer) session.save(employee);
-		
 		session.getTransaction().commit();
 		session.close();
-		
 		return id;
 	}
 
 	public List<Employee> getAllEmployees() {
 		
 		Session session = HibernateUtil.getSessionFactory().openSession();
-
 		session.beginTransaction();
-
 		@SuppressWarnings("unchecked")
 		List<Employee> empList = (List<Employee>) session.createQuery("FROM Employee e ORDER BY e.employeeName ASC").list();
-
 		session.getTransaction().commit();
 		session.close();
-
 		return empList;
 	}
 
 	public void updateEmployee(int id, int age) {
 		
 		Session session = HibernateUtil.getSessionFactory().openSession();
-
 		session.beginTransaction();
 		Employee employee = (Employee) session.get(Employee.class, id);
 		employee.setEmployeeAge(age);
-		// session.update(student);//no need to call update manually as it will be
-		// updated automatically on transaction close
 		session.getTransaction().commit();
 		session.close();
 	}
@@ -57,12 +45,9 @@ public int saveEmployee(String name, int age, double salary) {
 	public void updateEmployee(int id, double salary) {
 		
 		Session session = HibernateUtil.getSessionFactory().openSession();
-
 		session.beginTransaction();
 		Employee employee = (Employee) session.get(Employee.class, id);
 		employee.setEmployeeSalary(salary);
-		// session.update(student);//no need to call update manually as it will be
-		// updated automatically on transaction close
 		session.getTransaction().commit();
 		session.close();
 		
@@ -71,7 +56,6 @@ public int saveEmployee(String name, int age, double salary) {
 	public void deleteEmployee(int id) {
 		
 		Session session = HibernateUtil.getSessionFactory().openSession();
-
 		session.beginTransaction();
 		Employee employee = (Employee) session.get(Employee.class, id);
 		session.delete(employee);
@@ -82,15 +66,10 @@ public int saveEmployee(String name, int age, double salary) {
 	public Employee getEmployee(int id) {
 		
 		Session session = HibernateUtil.getSessionFactory().openSession();
-
 		session.beginTransaction();
-		
 		Employee employee = (Employee) session.get(Employee.class, id);
-//		session.delete(employee);
-		
 		session.getTransaction().commit();
 		session.close();
-		
 		return employee;
 	}
 }
